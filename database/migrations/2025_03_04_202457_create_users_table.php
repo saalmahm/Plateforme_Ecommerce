8f9262453757f458a2fe9907dad93067b437afa0
salma hamdi <saalmahm51@gmail.com>
2025-03-06 10:49:55 +0000

    Modifier la migration de la table des utilisateurs pour ajouter des colonnes 'nom', 'email', 'password' et 'role', et corriger la classe de migration.

--- database/migrations/2025_03_04_202457_create_users_table.php
+++ database/migrations/2025_03_04_202457_create_users_table.php
@@ -4,24 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('nom');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->enum('role', ['client', 'admin'])->default('client');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('users');
     }
-};
+}
